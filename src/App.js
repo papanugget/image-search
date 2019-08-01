eceaab90258662293e83f0a21f806e91bba9b70c
papanugget <evilgenius04@gmail.com>
2019-08-01 09:40:31 -0400

    scaffolding

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() {
+        return (
+            <div className="App">
+                Hello
+            </div>
+         );
+    }
+}
+
+export default App;
