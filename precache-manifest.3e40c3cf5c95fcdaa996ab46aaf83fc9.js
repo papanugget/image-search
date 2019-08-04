273ae06a01bba6818a00cf82e76d6c5cd662aca5
papanugget <evilgenius04@gmail.com>
2019-08-04 15:34:32 -0400

    Updates

---
+++ precache-manifest.3e40c3cf5c95fcdaa996ab46aaf83fc9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893fdbddeae2fc79dded0f559fd00d21",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "57dd140ccfad3df18916",
+    "url": "/image-search/static/js/2.d5f0efd0.chunk.js"
+  },
+  {
+    "revision": "ae148b44a2c50be32fe7",
+    "url": "/image-search/static/js/main.9055472b.chunk.js"
+  },
+  {
+    "revision": "b37db55bcc099aea1558",
+    "url": "/image-search/static/js/runtime~main.61dbcf7c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/image-search-react/precache-manifest.54d8cb1b32b988a4e975e1476f800973.js"
+  "/image-search/precache-manifest.3e40c3cf5c95fcdaa996ab46aaf83fc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/image-search-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/image-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
